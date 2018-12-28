c9e2c6764922360ebcaef49d12889c0981e5720b
Rory Magee <rory.magee2@gmail.com>
2018-12-28 22:08:59 +0000

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key: '3bef518d60a44edfbad144115182812'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key: '3bef518d60a44edfbad144115182812'
 };
 
 /*
